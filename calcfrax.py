e6ef6c2341a636523ddda6e35615d53ae05155f9
Dan Andrei Tutunea <dan.andrei.tutunea@icloud.com>
2016-12-03 20:26:22 +0000

    new file for the calc app

---
+++ calcfrax.py
@@ -0,0 +1,58 @@
+import sys
+from flask import Flask, jsonify, request
+
+a = 0
+b = 0
+result = 0
+op = ''
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+	return "Hello"
+
+@app.route('/postNumbers', methods=['POST'])
+def postNumere():
+    print(request.json)
+
+    global a
+    global b
+	a = request.json['a']
+	b = request.json['b']
+    global op
+    op = request.json['op']
+
+	return "got it"
+
+
+@app.route('/getResult', methods=['GET'])
+def getRezultat():
+
+    error='unknown operation
+    result=0
+
+    if op='+':
+        result=a+b
+        error='all ok'
+    if op='-':
+        result=a-b
+        error='all ok'
+    if op='*':
+        result=a*b
+        error='all ok'
+    if op='/':
+        if b==0:
+            error='divide by 0 impossible'
+        else:  
+            result=a/b
+            error='all ok'
+    
+    return jsonify({'result': result, 'error':error})
+
+
+
+
+
+
+if __name__ == '__main__':
+		app.run(debug=True)
