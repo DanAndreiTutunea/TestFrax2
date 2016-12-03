4c70f35039a5c4688ddc5fc65307a75679a7cb9c
Dan Andrei Tutunea <dan.andrei.tutunea@icloud.com>
2016-12-03 17:30:24 +0000

    aaa

--- testfrax.py
+++ testfrax.py
@@ -1,13 +1,61 @@
 import sys
+from flask import Flask, jsonify, request
 
-def main():
+a = 0
+b = 0	
+sum = 0
 
-	a=1
-	b=2
+app = Flask(__name__)
+@app.route('/')
+def index():
+	return "Hello"
 
-	c=a+b
-	print c
+@app.route('/getName', methods=['GET'])
+def func():
+	names = [
+		{
+			"id": 1,
+			"firstName": "Vlad",
+			"age": 19
+		},
+		{
+			"id": 2,
+			"firstName": "Dan",
+			"age": 30
+		}
+	]
+	return jsonify({'names': names})
 
 
+@app.route('/postNumbers', methods=['POST'])
+def getNumere():
+	print(request.json)
+	global a
+	global b
+	a = request.json['a']
+	b = request.json['b']
 
-main()
+	return "got it"
+
+
+@app.route('/Suma', methods=['GET'])
+def sum():
+	
+	global sum
+	sum = a + b
+
+	return jsonify({'sum': sum})
+
+
+#def main():
+#
+#	a=1
+#	b=2
+#
+#	c=a+b
+#	print c
+
+if __name__ == '__main__':
+		app.run(debug=True)
+
+#main()
